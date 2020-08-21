e7726ab3106774c9dc33798fe666444d1b4d3e20
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-08-20 20:31:17 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public static class Program
+    {
+        public static Character character = new Character();
+        public static SplashForm splashform;
+        public static GenerateNameForm generateNameForm;
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new AbilityGeneratorForm());
+            // create splashfrom and generatenameform objects
+            splashform = new SplashForm();
+            generateNameForm = new GenerateNameForm();
+
+            // the program starts with splashform
+            Application.Run(splashform);
+
+        }
+    }
+}
